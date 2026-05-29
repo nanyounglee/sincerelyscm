130e1532cfd1f95c1a1a441d075a4e9e5c396d7e
nanyounglee <nanyoung.lee@sincerely.one>
2026-05-29 17:19:03 +0900

    Delete scratch.py

--- scratch.py
+++
@@ -1,5 +0,0 @@
-import urllib.request, json
-req = urllib.request.Request('https://api.airtable.com/v0/appkRWtF2j99XgBTq/tblUYmhOvtHGJ9NO3?maxRecords=1&returnFieldsByFieldId=true', headers={'Authorization': 'Bearer patM0xqBHeF8WJIts'})
-res = urllib.request.urlopen(req)
-data = json.loads(res.read())
-print(json.dumps(data, indent=2))
